29a6c44ba4a55a86cfe7093bcb78e1ccafa1576a
GUILARF <58006841+GUILARF@users.noreply.github.com>
2019-12-14 14:37:31 -0300

    Criando modulos - Namespaces foram inseridos e retirados

--- ts/app.ts
+++ ts/app.ts
@@ -1,5 +1,8 @@
+alert('Oi');
+import { NegociacaoController } from './controllers/NegociacaoController';
 const controller = new NegociacaoController();
+$('.form').submit(controller.adiciona.bind(controller));
 
-document
-    .querySelector('.form')
-    .addEventListener('submit', controller.adiciona.bind(controller));
+// document
+//     .querySelector('.form')
+//     .addEventListener('submit', controller.adiciona.bind(controller));
